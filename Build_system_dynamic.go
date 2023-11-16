bda674cfdfbc0c9682374f2b42551ad95ec5effe
Sören Tempel <soeren+git@soeren-tempel.net>
2023-11-16 13:09:03 +0100

    Remove the version check for the dynamic build configuration
    Upstream is very stale at the moment and not bumping this regularly.
    However, at the time of writing git2go still works fine with the most
    recent libgit2 version. Therefore, just remove the version check for
    now until upstream work is resumed.

--- Build_system_dynamic.go
+++ Build_system_dynamic.go
@@ -8,8 +8,5 @@ package git
 #cgo CFLAGS: -DLIBGIT2_DYNAMIC
 #include <git2.h>
 
-#if LIBGIT2_VER_MAJOR != 1 || LIBGIT2_VER_MINOR < 5 || LIBGIT2_VER_MINOR > 5
-# error "Invalid libgit2 version; this git2go supports libgit2 between v1.5.0 and v1.5.0"
-#endif
 */
 import "C"
